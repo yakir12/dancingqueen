210719d38e7b6210adfbacfd97dc455e9810488e
Yakir Gagnon <12.yakir@gmail.com>
2023-08-10 15:04:09 +0200

    change julia ver to release

--- setup_rpi.sh
+++ setup_rpi.sh
@@ -1,10 +1,13 @@
 sudo apt-get update
 sudo apt-get -y dist-upgrade
+sudo reboot -h now
+# install programs
+sudo apt-get -y install git
 # install Julia
 curl -fsSL https://install.julialang.org | sh -s -- --yes --default-channel release
 # update bash
 . /home/pi/.bashrc
-# update Julia
-julia -e "import Pkg; Pkg.update()"
-
-sudo reboot -h now
+# setup software
+git clone https://github.com/yakir12/dancingqueen.git
+cd dancingqueen/pi
+julia --project=. -e "import Pkg; Pkg.instantiate()"
