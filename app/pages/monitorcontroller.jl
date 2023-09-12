69373bd54d2ecf3733b97bfef4d36ba7994456fd
Yakir Gagnon <12.yakir@gmail.com>
2023-09-12 20:31:43 +0200

    wip

--- app/pages/monitorcontroller.jl
+++ app/pages/monitorcontroller.jl
@@ -1,12 +1,17 @@
 # TODO:
-# add labels and current value to the sliders
-cell([ 
-      imageview(src=:imageurl, basic=true, style="max-width: $(Main.h)px")
-      toggle(:recording_label, :recording_on)
-     ],
-    [
-     slider(0:360, :bearing)
-    ],
-    [
-     slider(-1:0.1:1, :link_factor)
-    ])
+# maybe add ° to teh labels...
+
+cell([
+      row([
+           imageview(src=:imageurl, basic=true, style="max-width: $(Main.h)px")
+           toggle(:recording_label, :recording_on)
+          ])
+      row([
+           h6("Bearing")
+           slider(0:10:360, :bearing, markers=true, label=true, format='°')
+          ])
+      row([
+           h6("Link factor")
+           slider(-1:0.1:1, :link_factor, markers=true, label=true)
+          ])
+     ])
