b89a164b70e158741b440234c8da1a400fe41b01
Yakir Gagnon <12.yakir@gmail.com>
2023-10-03 00:04:39 +0200

    wip

--- app.jl
+++ app.jl
@@ -1,34 +1,48 @@
+module App
 using GenieFramework
+
 @genietools
 
 @app begin
-    @in slider_one = 1
-    @in slider_two = 11
+    @in chosen = ""
 
-    @onchange slider_one println(slider_one)
-    @onchange slider_two println(slider_two)
-
-    @out tabgroup = "one"
-    @out tabpanelgroup = "one"
+    @onchange chosen begin
+        println(chosen)
+    end
 end
+
 
 function ui()
-    [
-     tabgroup(:tabgroup, 
-              [
-               tab(name="one", label="Tab one"),
-               tab(name="two", label="Tab two")
-              ])
-     tabpanelgroup(:tabpanelgroup, 
-                   [
-                    tabpanel("Inside tab one", name = "one", [slider(1:10, :slider_one, markers=true, labelalways=true)])
-                    tabpanel("Inside tab two", name = "two", [slider(11:20, :slider_two, markers=true, labelalways=true)])
-                   ])
-    ]
+    Html.div(
+             @on("keydown.a", "chosen=\"a\""),
+             @on("keydown.b", "chosen=\"b\""),
+             @on("keydown.c", "chosen=\"c\""),
+             @on("keydown.d", "chosen=\"d\""),
+             @on("keydown.e", "chosen=\"e\""),
+             @on("keydown.f", "chosen=\"f\""),
+             @on("keydown.g", "chosen=\"g\""),
+             @on("keydown.h", "chosen=\"h\""),
+             @on("keydown.i", "chosen=\"i\""),
+             @on("keydown.j", "chosen=\"j\""),
+             @on("keydown.k", "chosen=\"k\""),
+             @on("keydown.l", "chosen=\"l\""),
+             @on("keydown.m", "chosen=\"m\""),
+             @on("keydown.n", "chosen=\"n\""),
+             @on("keydown.o", "chosen=\"o\""),
+             @on("keydown.p", "chosen=\"p\""),
+             @on("keydown.q", "chosen=\"q\""),
+             @on("keydown.r", "chosen=\"r\""),
+             @on("keydown.s", "chosen=\"s\""),
+             @on("keydown.t", "chosen=\"t\""),
+             @on("keydown.u", "chosen=\"u\""),
+             @on("keydown.v", "chosen=\"v\""),
+             @on("keydown.w", "chosen=\"w\""),
+             @on("keydown.x", "chosen=\"x\""),
+             @on("keydown.y", "chosen=\"y\""),
+             @on("keydown.z", "chosen=\"z\""),
+             [row(radio(msg, :chosen, val = msg)) for msg in ("a", "b", "c")]
+            )
 end
-
 @page("/", ui)
-
-Server.up()
-
+end
 
--- onepage/app.jl
+++ onepage/app.jl
@@ -5,6 +5,7 @@ using .DancingQueen
 using TOML
 using ImageDraw, CoordinateTransformations, Rotations, JpegTurbo, Colors, JSONSchema, JSONSchema
 import Colors.N0f8
+import DataStructures: OrderedDict
 
 include("display.jl")
 include("settings.jl")
@@ -16,6 +17,7 @@ route(get_frame, "/frame")
 
 route(respond ∘ get_recordings, "/data")
 
+const setups = Ref{OrderedDict{String, Vector{Sun}}}()
 
 
 Genie.config.cors_headers["Access-Control-Allow-Origin"]  =  "*"
@@ -26,11 +28,12 @@ route("/settings", method = POST) do
     files = Genie.Requests.filespayload()
     for file in files
         txt = String(last(file).data)
-        setups = try2settings(txt)
-        if setups isa String
-            @warn setups
+        _setups = try2settings(txt)
+        if _setups isa String
+            @warn _setups
         else
-            set_suns(setups[end].suns)
+            setups[] = _setups
+            set_suns(first(values(setups[])))
         end
     end
     if length(files) == 0
@@ -48,37 +51,72 @@ end
         set_recording(recording_on)
         recording_label = recording_on ? "Recording" : "Not recording"
     end
+
+    @in chosen = ""
+
+    @onchange chosen begin
+        # when keyboard is pressed the key will be wrong
+        set_suns(setups[][chosen])
+    end
+
 end myhandlers
 
-ui() = [
-        row([
-             btn(class = "q-mt-lg", "Download data", color = "primary", href="data", download=string(round(now(), Second(1)), ".tar"))
-            ])
-        row([
-             h1("DancingQueen")
-            ])
-        row([
-             card(class="st-col col-12", 
-                  [
-                   row([
-                        imageview(src=:imageurl, basic=true, style="max-width: $(h)px")
-                       ])
-                   row([
-                        toggle(:recording_label, :recording_on)
-                       ])
-                  ])
-            ])
-        row([
-             uploader(label="Upload settings", multiple=false, accept=".toml", method="POST", url="/settings", hideuploadbtn=false, nothumbnails=true, field__name="csv_file", autoupload=true)
-            ])
-       ]
+ui() = Html.div(
+                @on("keydown.a", "chosen=\"a\""),
+                @on("keydown.b", "chosen=\"b\""),
+                @on("keydown.c", "chosen=\"c\""),
+                @on("keydown.d", "chosen=\"d\""),
+                @on("keydown.e", "chosen=\"e\""),
+                @on("keydown.f", "chosen=\"f\""),
+                @on("keydown.g", "chosen=\"g\""),
+                @on("keydown.h", "chosen=\"h\""),
+                @on("keydown.i", "chosen=\"i\""),
+                @on("keydown.j", "chosen=\"j\""),
+                @on("keydown.k", "chosen=\"k\""),
+                @on("keydown.l", "chosen=\"l\""),
+                @on("keydown.m", "chosen=\"m\""),
+                @on("keydown.n", "chosen=\"n\""),
+                @on("keydown.o", "chosen=\"o\""),
+                @on("keydown.p", "chosen=\"p\""),
+                @on("keydown.q", "chosen=\"q\""),
+                @on("keydown.r", "chosen=\"r\""),
+                @on("keydown.s", "chosen=\"s\""),
+                @on("keydown.t", "chosen=\"t\""),
+                @on("keydown.u", "chosen=\"u\""),
+                @on("keydown.v", "chosen=\"v\""),
+                @on("keydown.w", "chosen=\"w\""),
+                @on("keydown.x", "chosen=\"x\""),
+                @on("keydown.y", "chosen=\"y\""),
+                @on("keydown.z", "chosen=\"z\""),
+                [row([
+                      btn(class = "q-mt-lg", "Download data", color = "primary", href="data", download=string(round(now(), Second(1)), ".tar"))
+                     ])
+                 row([
+                      h1("DancingQueen")
+                     ])
+                 row([
+                      card(class="st-col col-12", 
+                           [
+                            row([
+                                 imageview(src=:imageurl, basic=true, style="max-width: $(h)px")
+                                ])
+                            row([
+                                 toggle(:recording_label, :recording_on)
+                                ])
+                           ])
+                     ])
+                 row([
+                      uploader(label="Upload settings", multiple=false, accept=".toml", method="POST", url="/settings", hideuploadbtn=false, nothumbnails=true, field__name="csv_file", autoupload=true)
+                     ])
+                 row([row(radio(msg, :chosen, val = msg)) for msg in ("Off", "One stationary green sun", "Two different suns")])
+                ])
 
 model = init(FromFile, debounce = 0) |> myhandlers
 
 Stipple.js_methods(model::FromFile) = """
-    updateimage: async function () { 
-        this.imageurl = "frame#" + new Date().getTime()
-    }
+updateimage: async function () { 
+this.imageurl = "frame#" + new Date().getTime()
+}
 """
 
 # Stipple.js_created(model::FromFile) = """
