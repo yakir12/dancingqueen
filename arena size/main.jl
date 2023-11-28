16c9647d8745674f507c609a68ecedc8a7ccf98e
Yakir Gagnon <12.yakir@gmail.com>
2023-11-28 22:28:44 +0100

    wip

--- arena size/main.jl
+++ arena size/main.jl
@@ -1,31 +1,40 @@
-using GLMakie, CoordinateTransformations, LinearAlgebra, DataFrames
+using GLMakie, CoordinateTransformations, LinearAlgebra, DataFrames, Rotations
 
 angle(p1, p2) = atan(det(hcat(p1, p2)), p1 ⋅ p2)
 
 const r = 105 # cm
 const h = r
 
-# fig = Figure()
-# ax = Axis(fig[1,1], aspect=AxisAspect(1))
-# lines!(ax, Circle(zero(Point2f), r))
-# for i in 1:1
-function sample()
-    distance = r*rand()
+fig = Figure()
+ax = Axis(fig[1,1], aspect=DataAspect())
+lines!(ax, Circle(zero(Point2f), r), color = :gray)
+for i in 1:1
+# function sample()
+    distance = 0.5*r#*rand()
     beetle = CartesianFromPolar()(Polar(distance, 2π*rand()))
     sun = CartesianFromPolar()(Polar(r, 2π*rand()))
-    θ = PolarFromCartesian()(sun - beetle).θ
-    to_sun = Vec2f(cos(θ), sin(θ))
-    beetle_θ = 2π*rand()
-    beetle_direction = Vec2f(cos(beetle_θ), sin(beetle_θ))
-    Δ = angle(beetle_direction, to_sun)
-    return (; distance, Δ)
-    # scatter!(ax, Point2f[beetle, sun])
-    # arrows!(ax, [Point2f(beetle)], [to_sun]; fxaa=true, # turn on anti-aliasing
-    # linecolor = :gray, arrowcolor = :black,
-    # linewidth = 3, lengthscale=13)
+    beetle2sun = sun - beetle
+    beetle_θ = deg2rad(90)#2π*rand()
+    beetle_direction1 = Vec2f(cos(beetle_θ), sin(beetle_θ))
+    bearing1 = angle(beetle_direction1, beetle2sun)
+    @show rad2deg(bearing1)
+    Δ_beetle = deg2rad(45)#π*(2rand() - 1)
+    beetle_direction2 = LinearMap(Angle2d(Δ_beetle))(beetle_direction1)
+    bearing2 = angle(beetle_direction2, beetle2sun)
+    @show rad2deg(bearing2)
+    Δ = bearing1 - Δ_beetle - bearing2
+    @show rad2deg(Δ)
+    # return (; distance, Δ)
+    scatter!(ax, Point2f[beetle, sun])
+    arrows!(ax, [Point2f(beetle)], [beetle2sun]; fxaa=true, linecolor = :black, arrowcolor = :black, linewidth = 1, lengthscale=0.95)
+    arrows!(ax, [Point2f(beetle)], [beetle_direction1]; fxaa=true, linecolor = :green, arrowcolor = :green, linewidth = 1, lengthscale=r/2)
+    arrows!(ax, [Point2f(beetle)], [beetle_direction2]; fxaa=true, linecolor = :blue, arrowcolor = :blue, linewidth = 1, lengthscale=r/2)
+    # arrows!(ax, [Point2f(beetle)], [Vec2f(reverse(sincos(beetle_θ + bearing1)))]; fxaa=true, linecolor = :blue, arrowcolor = :blue, linewidth = 1, lengthscale=10)
+    # arrows!(ax, [Point2f(beetle)], [beetle_direction2]; fxaa=true, linecolor = :green, arrowcolor = :green, linewidth = 1, lengthscale=10)
+    # arrows!(ax, [Point2f(beetle)], [Vec2f(reverse(sincos(beetle_θ + bearing1 + Δ_beetle)))]; fxaa=true, linecolor = :red, arrowcolor = :red, linewidth = 1, lengthscale=15)
 end
 
-df = DataFrame((sample() for _ in 1:10000))
+df = DataFrame((sample() for _ in 1:100))
 
 scatter(df.distance, df.Δ)
 
