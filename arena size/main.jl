a4f3af2ee814ce8fc8df6ac92d3257c1a55a3b75
Yakir Gagnon <12.yakir@gmail.com>
2023-11-29 13:26:53 +0100

    arena size is 66 cm

--- arena size/main.jl
+++ arena size/main.jl
@@ -1,54 +1,33 @@
-using GLMakie, CoordinateTransformations, LinearAlgebra, DataFrames, Rotations
-
-angle(p1, p2) = atan(det(hcat(p1, p2)), p1 ⋅ p2)
+using CoordinateTransformations
 
 const r = 105 # cm
 const h = r
+n = 100
 
-fig = Figure()
-ax = Axis(fig[1,1], aspect=DataAspect())
-lines!(ax, Circle(zero(Point2f), r), color = :gray)
-for i in 1:1
-# function sample()
-    distance = 0.5*r#*rand()
+function sample(distance)
     beetle = CartesianFromPolar()(Polar(distance, 2π*rand()))
-    sun = CartesianFromPolar()(Polar(r, 2π*rand()))
-    beetle2sun = sun - beetle
-    beetle_θ = deg2rad(90)#2π*rand()
-    beetle_direction1 = Vec2f(cos(beetle_θ), sin(beetle_θ))
-    bearing1 = angle(beetle_direction1, beetle2sun)
-    @show rad2deg(bearing1)
-    Δ_beetle = deg2rad(45)#π*(2rand() - 1)
-    beetle_direction2 = LinearMap(Angle2d(Δ_beetle))(beetle_direction1)
-    bearing2 = angle(beetle_direction2, beetle2sun)
-    @show rad2deg(bearing2)
-    Δ = bearing1 - Δ_beetle - bearing2
-    @show rad2deg(Δ)
-    # return (; distance, Δ)
-    scatter!(ax, Point2f[beetle, sun])
-    arrows!(ax, [Point2f(beetle)], [beetle2sun]; fxaa=true, linecolor = :black, arrowcolor = :black, linewidth = 1, lengthscale=0.95)
-    arrows!(ax, [Point2f(beetle)], [beetle_direction1]; fxaa=true, linecolor = :green, arrowcolor = :green, linewidth = 1, lengthscale=r/2)
-    arrows!(ax, [Point2f(beetle)], [beetle_direction2]; fxaa=true, linecolor = :blue, arrowcolor = :blue, linewidth = 1, lengthscale=r/2)
-    # arrows!(ax, [Point2f(beetle)], [Vec2f(reverse(sincos(beetle_θ + bearing1)))]; fxaa=true, linecolor = :blue, arrowcolor = :blue, linewidth = 1, lengthscale=10)
-    # arrows!(ax, [Point2f(beetle)], [beetle_direction2]; fxaa=true, linecolor = :green, arrowcolor = :green, linewidth = 1, lengthscale=10)
-    # arrows!(ax, [Point2f(beetle)], [Vec2f(reverse(sincos(beetle_θ + bearing1 + Δ_beetle)))]; fxaa=true, linecolor = :red, arrowcolor = :red, linewidth = 1, lengthscale=15)
+    y = beetle.y
+    θ1 = asin(y/r)
+    sun = CartesianFromPolar()(Polar(r, θ1))
+    δs = range(-π, π, n + 1)[1:end-1]
+    θs = Vector{Float64}(undef, n)
+    for (i, δ) in enumerate(δs)
+        suni = CartesianFromPolar()(Polar(r, θ1+δ))
+        beetle2sun = suni - beetle
+        θs[i] = atan(reverse(beetle2sun)...)
+    end
+    a = diff(θs) .- step(δs)
+    for i in eachindex(a)
+        a[i] += a[i] > π ? -2π : a[i] < -π ? 2π : 0
+    end
+    Δ = maximum(abs, a)
 end
 
-df = DataFrame((sample() for _ in 1:100))
+distance = range(0, r, 1000)
+E = sample.(distance)
 
-scatter(df.distance, df.Δ)
+i = findfirst(>(deg2rad(6)), E)
+e = round(Int, distance[i])
 
+@show "The maximal radial distance (from the arena's center), where the worst angular error of the sun is bellow 6°, is at $e cm for a link factor of 1"
 
-function get_bearing(beetle, θ, sun)
-    s = SphericalFromCartesian()(CartesianFromSpherical()(sun) - CartesianFromSpherical()(beetle))
-    s.θ -θ
-end
-function sample(br, r, h)
-    beetle = Spherical(br, 2π*rand(), 0)
-    sun = Spherical(sqrt(r^2 + h^2), 2π*rand(), π/4)
-    # n = 1000
-    # θs = range(0, 2π, n + 1)[1:end-1]
-    # return [get_bearing(beetle, θ, sun) for θ in θs]
-end
-
-hist(rad2deg.(sample(r, h)), axis = (; limits=((-360, 360),nothing)))
