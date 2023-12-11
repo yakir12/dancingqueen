8b81c7ed9bc7f8b95f0dfb7e1b26ad04aed63153
Yakir Gagnon <12.yakir@gmail.com>
2023-12-11 14:26:54 +0100

    wip

--- new2/DancingQueen/src/camera.jl
+++ new2/DancingQueen/src/camera.jl
@@ -23,7 +23,7 @@ struct Camera
     w::Int
     h::Int
     function Camera()
-        w, h, fps = (1640, 1232, 30)
+        w, h, fps = (640, 480, 30)
         buff, view2img = create_buffer(w, h)
         cmd = `libcamera-vid -n --framerate $fps --width $w --height $h --timeout 0 --codec yuv420 -o -`
         o = open(cmd)
