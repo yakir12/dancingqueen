1fe5055f59f8f395dc653ed13ef45823a335d91b
Yakir Gagnon <12.yakir@gmail.com>
2024-02-12 17:16:26 +0100

    wip

--- new3/server/DancingQueen/src/DancingQueen.jl
+++ new3/server/DancingQueen/src/DancingQueen.jl
@@ -18,102 +18,147 @@ const tag_width = prefs["detection"]["tag_width"]
 const widen_radius = prefs["detection"]["widen_radius"]
 
 include("camera.jl")
-include("detector.jl")
-include("suns.jl")
-include("tracker.jl")
-include("leds.jl")
+# include("detector.jl")
+# include("suns.jl")
+# include("tracker.jl")
+# include("leds.jl")
 
 const off_sun = Dict("camera" => 0, "suns" => [Dict("link_factor" => 0)])
 
 
-mutable struct Controller
-    paused::Bool
-    condition::Condition
-end
-check(controller::Controller) = controller.paused && wait(controller.condition)
-pause!(controller::Controller) = (controller.paused = true)
-function resume!(controller::Controller) 
-    controller.paused = false
-    notify(controller.condition)
-end
-
-
-
-
-
-# const benchmark = Ref(now())
 #
-# function report_bm()
-#     t = now()
-#     Δ = t - benchmark[]
-#     fps = 1000 ÷ max(1, Dates.value(Δ))
-#     benchmark[] = t
-#     println(fps)
+#
+#
+#
+# # const benchmark = Ref(now())
+# #
+# # function report_bm()
+# #     t = now()
+# #     Δ = t - benchmark[]
+# #     fps = 1000 ÷ max(1, Dates.value(Δ))
+# #     benchmark[] = t
+# #     println(fps)
+# # end
+#
+# # get_image(cam::Ref{Camera}) = collect(reshape(rawview(channelview(cam[].img)), :))
+#
+# struct Instance{N, H}
+#     suns::NTuple{N, Sun}
+#     detector::DetectoRect{H}
+#     beetle::Ref{Union{Nothing, Beetle}}
+#     tracker::Track{N}
+#     leds::LEDs{N}
+#     running::Ref{Bool}
+#     task::Task
+#     function Instance(cam::Camera{H}, suns::NTuple{N, Sun}) where {N, H}
+#         detector = DetectoRect(cam, camera_distance, tag_width, widen_radius)
+#         beetle = Ref{Union{Nothing, Beetle}}(nothing)
+#         tracker = Track(suns)
+#         leds = LEDs(baudrate, suns)
+#         running = Ref(true)
+#         task = Threads.@spawn while running[]
+#             one_iter(cam, detector, beetle, tracker, leds)
+#             # report_bm()
+#             yield()
+#         end
+#         new{N, H}(suns, detector, beetle, tracker, leds, running, task)
+#     end
+# end
+# # Instance(cam::Camera{H}, suns::NTuple{N, Sun}) where {N, H} = Instance{N, H}(cam, suns)
+# Instance(cam, suns) = Instance(cam, Tuple(Sun.(suns)))
+#
+# function one_iter(cam, detector, beetle, tracker, leds)
+#     beetle[] = detector(snap(cam))
+#     tracker(beetle[])
+#     update_suns!(tracker)
+#     leds(tracker.sun_θs)
+# end
+#
+# function stop(i::Instance)
+#     if i.running[]
+#         i.running[] = false
+#         wait(i.task)
+#         close(i.detector)
+#         close(i.leds)
+#     end
+# end
+#
+# function main()
+#     setup = Observable(off_sun; ignore_equal_values = true)
+#     cam = Ref(Camera(setup[]))
+#     instance = Ref{Instance}(Instance(cam[], setup[]["suns"]))
+#     on(setup) do setup
+#         pause!(resume)
+#         switch!(cam, setup)
+#         instance[] = Instance(cam[], setup["suns"])
+#         play!(resume)
+#     end
+#     get_bytes() = vec(cam[].img)
+#     get_state() = (datetime = now(), rect = instance[].detector.rect, beetle = instance[].beetle[], leds = instance[].leds.msg)
+#     return setup, get_bytes, get_state
 # end
 
-# get_image(cam::Ref{Camera}) = collect(reshape(rawview(channelview(cam[].img)), :))
+mutable struct CheckPoint
+    paused::Bool
+    lock::ReentrantLock
+    condition::Threads.Condition
+    function CheckPoint() 
+        lock = ReentrantLock()
+        new(true, lock, Threads.Condition(lock))
+    end
+end
+function check(cp::CheckPoint) 
+    @lock cp.lock begin
+        cp.paused && wait(cp.condition)
+    end
+end
 
-struct Instance{N, H}
-    suns::NTuple{N, Sun}
-    detector::DetectoRect{H}
-    beetle::Ref{Union{Nothing, Beetle}}
-    tracker::Track{N}
-    leds::LEDs{N}
-    running::Ref{Bool}
+function pause!(cp::CheckPoint) 
+    @lock cp.lock begin
+        cp.paused = true
+    end
+end
+
+function resume!(cp::CheckPoint) 
+    @lock cp.lock begin
+        cp.paused = false
+        notify(cp.condition)
+    end
+end
+
+
+struct Instance
+    lock::ReentrantLock
+    checkpoint::CheckPoint
+    camera::Ref{Camera}
     task::Task
-    function Instance(cam::Camera{H}, suns::NTuple{N, Sun}) where {N, H}
-        detector = DetectoRect(cam, camera_distance, tag_width, widen_radius)
-        beetle = Ref{Union{Nothing, Beetle}}(nothing)
-        tracker = Track(suns)
-        leds = LEDs(baudrate, suns)
-        running = Ref(true)
-        task = Threads.@spawn while running[]
-            one_iter(cam, detector, beetle, tracker, leds)
-            # report_bm()
-            yield()
+    function Instance()
+        cam = Ref(Camera(cmoff))
+        checkpoint = CheckPoint()
+        task = Threads.@spawn while true
+            check(checkpoint)
+            snap(cam[])
         end
-        new{N, H}(suns, detector, beetle, tracker, leds, running, task)
-    end
-end
-# Instance(cam::Camera{H}, suns::NTuple{N, Sun}) where {N, H} = Instance{N, H}(cam, suns)
-Instance(cam, suns) = Instance(cam, Tuple(Sun.(suns)))
-
-function one_iter(cam, detector, beetle, tracker, leds)
-    beetle[] = detector(snap(cam))
-    tracker(beetle[])
-    update_suns!(tracker)
-    leds(tracker.sun_θs)
-end
-
-function stop(i::Instance)
-    if i.running[]
-        i.running[] = false
-        wait(i.task)
-        close(i.detector)
-        close(i.leds)
+        new(ReentrantLock(), checkpoint, cam, task)
     end
 end
 
-function main()
-    setup = Observable(off_sun; ignore_equal_values = true)
-    cam = Ref(Camera(setup[]))
-    instance = Ref{Instance}(Instance(cam[], setup[]["suns"]))
-    on(setup) do setup
-        pause!(resume)
-        switch!(cam, setup)
-        instance[] = Instance(cam[], setup["suns"])
-        play!(resume)
+function update!(instance::Instance, setup::Dict)
+    pause!(instance.checkpoint)
+    switch!(instance.camera, setup)
+    if instance.camera[].cm ≠ cmoff
+        resume!(instance.checkpoint)
     end
-    get_bytes() = vec(cam[].img)
-    get_state() = (datetime = now(), rect = instance[].detector.rect, beetle = instance[].beetle[], leds = instance[].leds.msg)
-    return setup, get_bytes, get_state
 end
 
 function main()
     setup = Observable(off_sun)
-    instance = Instance(setup[])
-    on(setup -> update!(instance, setup), setup)
-    return setup, () -> get_bytes(instance), () -> get_state(instance)
+    instance = Instance()
+    on(setup) do setup
+        @lock instance.lock update!(instance, setup)
+    end
+    return setup, () -> collect(instance.camera[].img), instance
+    # return setup, () -> get_bytes(instance), () -> get_state(instance)
 end
 
 end # module DancingQueen
--- new3/server/DancingQueen/src/camera.jl
+++ new3/server/DancingQueen/src/camera.jl
@@ -1,83 +1,99 @@
-const camera_lock = ReentrantLock()
-
 @enum CamMode cmoff=0 cm2464=2464 cm1080=1080 cm1232=1232 cm480=480
 
-camera_settings(cm::CamMode) = cm == cm480 ? (w = 640, h = 480, fps = 206) :
-                               cm == cm1232 ? (w = 1640, h = 1232, fps = 83) :
-                               cm == cm1080 ? (w = 1920, h = 1080, fps = 47) :
-                               cm == cm2464 ? (w = 3280, h = 2464, fps = 21) :
-                               (w = 640, h = 480, fps = 10)
-
-function get_buffer_img(w, h)
-    w2 = 64ceil(Int, w/64) # dimension adjustments to hardware restrictions
-    nb = Int(w2*h*3/2) # total number of bytes per img
-    buff = Vector{UInt8}(undef, nb)
-    i1 = (w - h) ÷ 2
-    i2 = i1 + h - 1
-    img = view(reshape(view(buff, 1:w2*h), w2, h), i1:i2, h:-1:1)
-    return buff, img
-end
-
 struct Camera
-    mode::CamMode
-    buff::Vector{UInt8}
-    img::SubArray{UInt8, 2, Base.ReshapedArray{UInt8, 2, SubArray{UInt8, 1, Vector{UInt8}, Tuple{UnitRange{Int64}}, true}, Tuple{}}, Tuple{UnitRange{Int64}, StepRange{Int64, Int64}}, false}
-    proc::Base.Process
-    function Camera(cm::CamMode)
-        w, h, fps = camera_settings(cm)
-        buff, img = get_buffer_img(w, h)
-        if cm == cmoff
-            proc = open(`echo closing camera`)
-            close(proc)
-        else
-            proc = open(`rpicam-vid --denoise cdn_off -n --framerate $fps --width $w --height $h --timeout 0 --codec yuv420 -o -`)
-        end
-        new(cm, buff, img, proc)
-    end
+    cm::CamMode
+    img::Matrix{Float64}
+    Camera(cm::CamMode) = new(cm, zeros(Float64, Int(cm), Int(cm)))
 end
-
 function snap(cam::Camera) 
-    read!(cam.proc, cam.buff)
+    sleep(1)
+    cam.img .= rand(Float64, Int(cam.cm), Int(cam.cm))
     return cam.img
 end
 
-Base.size(cam::Camera) = Int(cam.mode)
-
-
-#TODO: test to see which is better, isopen with kill or close for cam.proc with wait or what
-# shutdown is not cammode == 0 
-
-Base.isopen(cam::Camera) = isopen(cam.o)
-
 function Base.close(cam::Camera) 
-    if isopen(cam)
-        close(cam.o)
-        wait(cam.o)
-    end
+    nothing
 end
 
+###############
+#
+# camera_settings(cm::CamMode) = cm == cm480 ? (w = 640, h = 480, fps = 206) :
+#                                cm == cm1232 ? (w = 1640, h = 1232, fps = 83) :
+#                                cm == cm1080 ? (w = 1920, h = 1080, fps = 47) :
+#                                cm == cm2464 ? (w = 3280, h = 2464, fps = 21) :
+#                                (w = 640, h = 480, fps = 10)
+#
+# function get_buffer_img(w, h)
+#     w2 = 64ceil(Int, w/64) # dimension adjustments to hardware restrictions
+#     nb = Int(w2*h*3/2) # total number of bytes per img
+#     buff = Vector{UInt8}(undef, nb)
+#     i1 = (w - h) ÷ 2
+#     i2 = i1 + h - 1
+#     img = view(reshape(view(buff, 1:w2*h), w2, h), i1:i2, h:-1:1)
+#     return buff, img
+# end
+#
+# struct Camera
+#     mode::CamMode
+#     buff::Vector{UInt8}
+#     img::SubArray{UInt8, 2, Base.ReshapedArray{UInt8, 2, SubArray{UInt8, 1, Vector{UInt8}, Tuple{UnitRange{Int64}}, true}, Tuple{}}, Tuple{UnitRange{Int64}, StepRange{Int64, Int64}}, false}
+#     proc::Base.Process
+#     function Camera(cm::CamMode)
+#         w, h, fps = camera_settings(cm)
+#         buff, img = get_buffer_img(w, h)
+#         if cm == cmoff
+#             proc = open(`echo closing camera`)
+#             close(proc)
+#         else
+#             proc = open(`rpicam-vid --denoise cdn_off -n --framerate $fps --width $w --height $h --timeout 0 --codec yuv420 -o -`)
+#         end
+#         new(cm, buff, img, proc)
+#     end
+# end
+#
+# function snap(cam::Camera) 
+#     read!(cam.proc, cam.buff)
+#     return cam.img
+# end
+#
+# Base.size(cam::Camera) = Int(cam.mode)
+#
+#
+# #TODO: test to see which is better, isopen with kill or close for cam.proc with wait or what
+# # shutdown is now cammode == 0 
+#
+# Base.isopen(cam::Camera) = isopen(cam.o)
+#
+# function Base.close(cam::Camera) 
+#     if isopen(cam)
+#         close(cam.o)
+#         wait(cam.o)
+#     end
+# end
+#
 CamMode(setup::Dict) = CamMode(get(setup, "camera", 1080))
 
 Camera(setup::Dict) = Camera(CamMode(setup))
 
-function switch!(cam::Ref{Camera}, setup::Dict)
+function switch!(current::Ref{Camera}, setup::Dict)
     new_cm = CamMode(setup)
-    cam[].cm == new_cm && return nothing
-    close(cam[])
-    cam[] = Camera(new_cm)
+    if current[].cm ≠ new_cm
+        close(current[])
+        current[] = Camera(new_cm)
+    end
 end
-
-
-##### the only four options:
-# w, h, fps = (3280, 2464, 21) # 
-# w, h, fps = (1920, 1080, 47) # same high resolution as 3280
-# w, h, fps = (1640, 1232, 83) # same low resolution as 640
-# w, h, fps = (640, 480, 206)
-
-get_camera_fov(h::Int) = 
-h == 480 ? 480/1232*48.8 :
-h == 1232 ? 48.8 :
-h == 1080 ? 1080/2464*48.8 :
-h == 2464 ? 48.8 :
-nothing
-
+#
+#
+# ##### the only four options:
+# # w, h, fps = (3280, 2464, 21) # 
+# # w, h, fps = (1920, 1080, 47) # same high resolution as 3280
+# # w, h, fps = (1640, 1232, 83) # same low resolution as 640
+# # w, h, fps = (640, 480, 206)
+#
+# get_camera_fov(h::Int) = 
+# h == 480 ? 480/1232*48.8 :
+# h == 1232 ? 48.8 :
+# h == 1080 ? 1080/2464*48.8 :
+# h == 2464 ? 48.8 :
+# nothing
+#
